747e6e906380f02dc8b3466305b67ebca1403ffa
n0tsiege <dachaitkin@gmail.com>
2021-06-28 09:16:23 -0400

    feat(site): Added old files back for archival

--- pages/500.js
+++
@@ -1,3 +0,0 @@
-export default function Custom500() {
-    return <h1>500 - The server seems to be broken lol report this to our <a href="https://discord.gg/vgYmzjJJtZ">discord</a> or wait 10 minutes.</h1>
-  }
